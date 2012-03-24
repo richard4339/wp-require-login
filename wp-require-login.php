c0125b4fd07d07ac7b39c7bc8556d84aa974588d
Richard <richard@mozor.net>
2012-03-24 14:36:18 -0500

    Added uninstall/activation hooks, closes #3

--- wp-require-login.php
+++ wp-require-login.php
@@ -3,7 +3,7 @@
   Plugin Name: Require Login
   Plugin URI: https://github.com/richard4339/wp-require-login
   Description: Plugin redirects users to the login page whenever they try to visit any page on the website.
-  Version: 0.5
+  Version: 1.0
   Author: Richard
   Author URI: http://www.digitalxero.com
  */
@@ -23,6 +23,27 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+register_uninstall_hook(__FILE__, 'rl_uninstall');
+register_activation_hook(__FILE__, 'rl_activate');
+
+function rl_uninstall() {
+    delete_option('require_login');
+    delete_option('require_login_first_notice');
+}
+
+function rl_activate() {
+    add_option('require_login', '0');
+    add_option('require_login_first_notice', '1');
+}
+
+function rl_post_install() {
+    if (get_option('require_login_first_notice') == '1') {
+        echo '<div class="updated"><p>Click <a href="'.get_admin_url(null, 'options-general.php?page=require_login').'">here</a> to turn on Require Login.</p></div>';
+        delete_option('require_login_first_notice');
+    }
+}
+
+add_action('admin_notices', 'rl_post_install');
 add_action('admin_menu', 'rl_menu');
 
 function rl_menu() {
@@ -31,15 +52,15 @@ function rl_menu() {
 
 function require_login() {
     ?>
-<div>
-    <h2><?php print __('Require Login', 'require_login'); ?></h2>
-    <form method="post" action="options.php">
-        <?php settings_fields('require_login'); ?>
-        <?php do_settings_sections('require_login'); ?>
+    <div>
+        <h2><?php print __('Require Login', 'require_login'); ?></h2>
+        <form method="post" action="options.php">
+            <?php settings_fields('require_login'); ?>
+            <?php do_settings_sections('require_login'); ?>
 
-        <input type="submit" name="Submit" value="<?php _e('Update Options ?') ?>" />
-    </form>
-</div>
+            <input type="submit" name="Submit" value="<?php _e('Update Options ?') ?>" />
+        </form>
+    </div>
 
     <?php
 }
@@ -56,13 +77,17 @@ function plugin_admin_init() {
 
 function rl_section_text() {
     echo '<p>Settings for the Require Login Plugin. This option to require a login for the site is currently <b>';
-    if(get_option('require_login') != '1') { print 'off'; } else { print 'on'; }
+    if (get_option('require_login') != '1') {
+        print 'off';
+    } else {
+        print 'on';
+    }
     print '</b>.</p>';
 }
 
 function rl_setting_string() {
     ?>
-    <input name="require_login" id="rl_require_login" type="checkbox" value="1" class="code" <?php print checked( 1, get_option('require_login'), false ); ?> />
+    <input name="require_login" id="rl_require_login" type="checkbox" value="1" class="code" <?php print checked(1, get_option('require_login'), false); ?> />
     <?php
 }
 
